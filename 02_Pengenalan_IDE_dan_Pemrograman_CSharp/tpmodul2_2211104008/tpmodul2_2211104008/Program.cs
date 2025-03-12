7883aa55f09074de22dafba609674491b6431f22
Maria <nathasyamaria80@gmail.com>
2025-03-12 21:33:58 +0700

    Menambahkan Tugas Pendahuluan Modul 2

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_CSharp/tpmodul2_2211104008/tpmodul2_2211104008/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Bagian A: Menerima input huruf dan menentukan vokal atau konsonan
+        Console.Write("Masukkan satu huruf: ");
+        char huruf = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+
+        if ("AIUEO".Contains(huruf))
+        {
+            Console.WriteLine($"Huruf {huruf} merupakan huruf vokal");
+        }
+        else if (char.IsLetter(huruf))
+        {
+            Console.WriteLine($"Huruf {huruf} merupakan huruf konsonan");
+        }
+        else
+        {
+            Console.WriteLine("Input bukan huruf yang valid!");
+        }
+
+        // Bagian B: Membuat array bilangan genap dan menampilkan isi array
+        int[] bilanganGenap = { 2, 4, 6, 8, 10 };
+
+        for (int i = 0; i < bilanganGenap.Length; i++)
+        {
+            Console.WriteLine($"Angka genap {i + 1} : {bilanganGenap[i]}");
+        }
+    }
+}
