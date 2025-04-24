06f79e63ff2a3e2a79ea8bff02329dd94f65fdb1
Maria <nathasyamaria80@gmail.com>
2025-04-24 16:20:29 +0700

    Upload TP Modul 5 - Menambahkan generic method

---
+++ 05_Generics/tpmodul5_2211104008/tpmodul5_2211104008/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul5_2211104008
+{
+    class HaloGeneric
+    {
+        public void SapaUser<T>(T user)
+        {
+            Console.WriteLine($"Halo user {user}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HaloGeneric halo = new HaloGeneric();
+            halo.SapaUser("Maria Nathasya Desfera Pangestu");
+        }
+    }
+}
