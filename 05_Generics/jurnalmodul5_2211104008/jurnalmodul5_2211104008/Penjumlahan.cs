41c74511f49ec5dc4a53ec15d127758968bc56dc
Maria Nathasya <nathasyamaria80@gmail.com>
2025-06-10 01:12:06 +0700

    menambahkan class Penjumlahan dengan method JumlahTigaAngka

---
+++ 05_Generics/jurnalmodul5_2211104008/jurnalmodul5_2211104008/Penjumlahan.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Penjumlahan
+{
+    public static T JumlahTigaAngka<T>(T angka1, T angka2, T angka3)
+    {
+        dynamic a = angka1, b = angka2, c = angka3;
+        return (T)(a + b + c);
+    }
+}
--- 05_Generics/jurnalmodul5_2211104008/jurnalmodul5_2211104008/Program.cs
+++ 05_Generics/jurnalmodul5_2211104008/jurnalmodul5_2211104008/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Menggunakan tiga angka dari dua digit NIM: 22, 11, dan 08
+        int angka1 = 22;
+        int angka2 = 11;
+        int angka3 = 08;
+
+        int hasil = Penjumlahan.JumlahTigaAngka(angka1, angka2, angka3);
+        Console.WriteLine("NIM 2211104008");
+        Console.WriteLine("Penjumlahan Tiga Angka dari NIM saya (22+11+08)");
+        Console.WriteLine($"\nHasil penjumlahan: {hasil}");
+    }
+}
