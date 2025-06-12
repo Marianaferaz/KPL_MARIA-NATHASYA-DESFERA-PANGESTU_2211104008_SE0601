d6129463c1108abb0322811cfe854c26041724a2
Maria Nathasya <nathasyamaria80@gmail.com>
2025-06-12 20:37:03 +0700

    “menambahkan API mahasiswa”

---
+++ 09_API_Design_dan_Construction_Using_Swagger/tp_modul9_api/tp_modul9_api/Controllers/MahasiswaController.cs.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using tp_modul9.Models;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MahasiswaController : ControllerBase
+{
+    private static List<Mahasiswa> listMahasiswa = new List<Mahasiswa>
+    {
+        new Mahasiswa { Nama = "Maria Nathasya", Nim = "2211104008" },
+        new Mahasiswa { Nama = "Alfian Mutakim", Nim = "2211104017" },
+        new Mahasiswa { Nama = "Nadia Putri Rahmaniar", Nim = "2211104012" },
+        new Mahasiswa { Nama = "Muhammad Edgar Nadhif", Nim = "2211104028" },
+        new Mahasiswa { Nama = "Muhammad Dhimas Afrizal", Nim = "2211104025" },
+    };
+
+    [HttpGet]
+    public IEnumerable<Mahasiswa> Get()
+    {
+        return listMahasiswa;
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Mahasiswa> Get(int id)
+    {
+        if (id < 0 || id >= listMahasiswa.Count)
+            return NotFound();
+        return listMahasiswa[id];
+    }
+
+    [HttpPost]
+    public void Post([FromBody] Mahasiswa mhs)
+    {
+        listMahasiswa.Add(mhs);
+    }
+
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+        if (id >= 0 && id < listMahasiswa.Count)
+            listMahasiswa.RemoveAt(id);
+    }
+}
---
+++ 09_API_Design_dan_Construction_Using_Swagger/tp_modul9_api/tp_modul9_api/Models/Mahasiswa.cs
@@ -0,0 +1,8 @@
+﻿namespace tp_modul9.Models
+{
+    public class Mahasiswa
+    {
+        public string Nama { get; set; }
+        public string Nim { get; set; }
+    }
+}
