39c0e2a5df28ade1933b9380cce5fd43a40dd904
Maria Nathasya <nathasyamaria80@gmail.com>
2025-06-12 23:45:58 +0700

    push jurnal 9

---
+++ 09_API_Design_dan_Construction_Using_Swagger/JURNAL/Movies.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace modul9_2211104008
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public List<string> Stars { get; set; }
+        public string Description { get; set; }
+
+        public Movie() { }
+    }
+}
