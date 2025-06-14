ff5a13c5036abcd3dfa7c409e4e85f62c7662b31
Maria Nathasya <nathasyamaria80@gmail.com>
2025-06-14 22:34:12 +0700

    push TP Modul 14

---
+++ 14_Clean_Code/TP/tpmodul14_2211104008/tpmodul14_2211104008/tpmodul14_2211104008.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class GreetingService
+{
+    public static void GreetUser<T>(T user)
+    {
+        Console.WriteLine($"Halo user {user}");
+    }
+}
+
+public class GenericData<T>
+{
+    private readonly T _data;
+
+    public GenericData(T data)
+    {
+        _data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: {_data}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Memanggil method untuk menyapa pengguna
+        GreetingService.GreetUser("Maria Nathasya");
+
+        // Membuat objek GenericData dengan tipe string (NIM)
+        var nimData = new GenericData<string>("2211104008");
+        nimData.PrintData();
+    }
+}
